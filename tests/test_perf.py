18462db2d01238332a49f607c7999881de1af784
Samuel Howard <sahoward42@tntech.edu>
2024-03-07 14:49:33 -0600

    Added performance testing
    Added performance testing to optimize query speed

---
+++ tests/test_perf.py
@@ -0,0 +1,22 @@
+from src.bible_search.bible_search import BibleSearch
+import time
+import unittest
+
+
+class TestPerf(unittest.TestCase):
+    def setUp(self) -> None:
+        self.bible_search = BibleSearch()
+
+    def test_perf(self):
+        count = 2000
+        start = time.perf_counter()
+        for i in range(count):
+            # Do some searching
+            self.bible_search.search("Jesus wept")
+        end = time.perf_counter()
+        print(f"Total: {end - start:.4f}s\n"
+              f"{(end - start) / count:.8f}s per search average")
+
+
+if __name__ == '__main__':
+    unittest.main()

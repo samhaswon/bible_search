437ddd1054d04079245688dd3e58aa9d6f57ba85
Samuel Howard <sahoward42@tntech.edu>
2024-03-07 12:21:24 -0600

    Forgo repeated object creation for efficiency

--- src/translate.py
+++ src/translate.py
@@ -2,14 +2,7 @@
 Translation layer
 """
 
-
-def translate(book: str) -> int:
-    """
-    Get the (from 1) index of a book of the Bible.
-    :param book: book name.
-    :return: index of the book.
-    """
-    books = {
+books = {
         'Genesis': 1, 'Exodus': 2, 'Leviticus': 3, 'Numbers': 4, 'Deuteronomy': 5, 'Joshua': 6,
         'Judges': 7, 'Ruth': 8, '1 Samuel': 9, '2 Samuel': 10, '1 Kings': 11, '2 Kings': 12, '1 Chronicles': 13,
         '2 Chronicles': 14, 'Ezra': 15, 'Nehemiah': 16, 'Esther': 17, 'Job': 18, 'Psalms': 19, 'Proverbs': 20,
@@ -21,16 +14,8 @@ def translate(book: str) -> int:
         '1 Timothy': 54, '2 Timothy': 55, 'Titus': 56, 'Philemon': 57, 'Hebrews': 58, 'James': 59, '1 Peter': 60,
         '2 Peter': 61, '1 John': 62, '2 John': 63, '3 John': 64, 'Jude': 65, 'Revelation': 66
     }
-    return books[book]
 
-
-def rtranslate(book: int) -> str:
-    """
-    Gets the name of a book based on its index. Opposite of translate()
-    :param book: index of book name to find.
-    :return: book name.
-    """
-    books = {
+rbooks = {
         1: 'Genesis', 2: 'Exodus', 3: 'Leviticus', 4: 'Numbers', 5: 'Deuteronomy', 6: 'Joshua', 7: 'Judges', 8: 'Ruth',
         9: '1 Samuel', 10: '2 Samuel', 11: '1 Kings', 12: '2 Kings', 13: '1 Chronicles', 14: '2 Chronicles', 15: 'Ezra',
         16: 'Nehemiah', 17: 'Esther', 18: 'Job', 19: 'Psalms', 20: 'Proverbs', 21: 'Ecclesiastes',
@@ -42,4 +27,21 @@ def rtranslate(book: int) -> str:
         54: '1 Timothy', 55: '2 Timothy', 56: 'Titus', 57: 'Philemon', 58: 'Hebrews', 59: 'James', 60: '1 Peter',
         61: '2 Peter', 62: '1 John', 63: '2 John', 64: '3 John', 65: 'Jude', 66: 'Revelation'
     }
+
+
+def translate(book: str) -> int:
+    """
+    Get the (from 1) index of a book of the Bible.
+    :param book: book name.
+    :return: index of the book.
+    """
     return books[book]
+
+
+def rtranslate(book: int) -> str:
+    """
+    Gets the name of a book based on its index. Opposite of translate()
+    :param book: index of book name to find.
+    :return: book name.
+    """
+    return rbooks[book]

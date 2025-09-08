db9c979c53a02b93412f52b0fd677fcc642d9600
Samuel Howard <sahoward42@tntech.edu>
2025-09-08 10:32:30 -0500

    Reformat flags

--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ import os
 # pylint: disable=import-error
 from setuptools import setup, Extension
 
-
 if os.name == "nt":
     # MSVC flags
-    flags = ["/favor:blend",  # Optimize for AMD and Intel
-             "/GF",  # (Eliminate Duplicate Strings)
-             "/Qpar",  # (Auto-parallelizer)
-             "/arch:AVX2",  # At this point, this has a negligible impact
-             "/fp:fast"  # Faster floating point ops, since precision is unnecessary for this
-             ]
+    flags = [
+        "/favor:blend",  # Optimize for AMD and Intel
+        "/GF",           # (Eliminate Duplicate Strings)
+        "/Qpar",         # (Auto-parallelizer)
+        "/arch:AVX2",    # At this point, this has a negligible impact
+        "/fp:fast",      # Faster floating point ops, since precision is unnecessary for this
+    ]
 else:
     flags = ["-O3"]
 
